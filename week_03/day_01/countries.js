65844f918098e82aced103fd4e10ddeac25f89b3
manishnetmeet <manishnetmeet101@gmail.com>
2020-05-04 22:41:07 +0530

    passed

--- week_03/day_01/countries.js
+++ week_03/day_01/countries.js
@@ -1,12 +1,41 @@
 function program(input) {
-    // input
-    // console.log(input)
-    // write code here
+  // input
+  // console.log(input)
+  // write code here
+  switch (input) {
+    case "India":
+    case "China":
+    case "Nepal":
+    case "Singapore":
+      return "Asia";
 
+    case "Canada":
+    case "United States of America":
+    case "Mexico":
+      return "North America";
 
-    // return output
-    return 'output'
+    case "France":
+    case "Germany":
+    case "Sweden":
+      return "Europe";
+
+    case "South Africa":
+    case "Nigeria":
+      return "Africa";
+
+    case "Australia":
+      return "Australia";
+
+    case "Argentina":
+    case "Brazil":
+      return "South America";
+
+    default:
+      return "Invalid";
+  }
+
+  //   // return output
+  //   return "output";
 }
 
-
-module.exports = program
+module.exports = program;
